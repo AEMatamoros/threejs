02764d8bda8cab68335d920d0e5d4f470c336eaf
AEMatamoros <alexismatamoros1996888@gmail.com>
2024-03-25 15:19:52 -0600

    First Commit

---
+++ Lesson 01 - Basic Scene and Object/src/script.js
@@ -0,0 +1,36 @@
+import * as THREE from "three";
+const canvas = document.querySelector("canvas.webgl");
+
+//Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1); //Width, Height, Deep
+
+const material = new THREE.MeshBasicMaterial({ color: "teal" });
+
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); //Degress if view, aspect radio,
+// camera.position.x;
+// camera.position.y;
+camera.position.z = 3;
+scene.add(camera);
+
+//Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
